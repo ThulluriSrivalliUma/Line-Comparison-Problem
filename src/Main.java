9f89a84a433b89cd0d956a850861f7b4cd527828
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-09 00:05:24 +0530

    Line length

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the coordinate of the first Line : (x1 y1) (x2 y2)");
+        int x1= sc.nextInt();
+        int y1= sc.nextInt();
+        int x2= sc.nextInt();
+        int y2= sc.nextInt();
+        Line l1=new Line(x1,y1,x2,y2);
+        l1.getLength();
+
     }
 }
