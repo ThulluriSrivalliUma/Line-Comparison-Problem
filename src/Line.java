c05841bed38a5f8c865a505d953e8d80ea6bb2a8
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-16 14:28:54 +0530

    CompareTo  method usage

--- src/Line.java
+++ src/Line.java
@@ -1,15 +1,19 @@
 public class Line {
-    Point p1;
-    Point p2;
-    double length;
+    private Point p1;
+    private Point p2;
+    private Double length;
 
     Line(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
+        calculateLength();
     }
 
-    public void calculateLength() {
+    private void calculateLength() {
         this.length = Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
         System.out.println("The length of the line is: " + this.length);
+    }
+    public int compareTo(Line two) {
+        return this.length.compareTo(two.length);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
         Main m1= new Main();
         Line l1=new Line(m1.getPoint(),m1.getPoint());
-        l1.calculateLength();
         Line l2=new Line(m1.getPoint(),m1.getPoint());
-        l2.calculateLength();
-        if(l1.length>l2.length){
+        int result = l1.compareTo(l2);
+        if(result>0){
             System.out.println("Line 1 is greater than Line 2");
-        } else if (l1.length<l2.length) {
+        } else if (result<0) {
             System.out.println("Line 1 is smaller than Line 2");
         } else{
             System.out.println("Line 1 and Line 2 are equal");
